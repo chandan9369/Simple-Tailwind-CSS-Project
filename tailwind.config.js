90d8dd38d30386bc1443359fba4bba123735a9e5
chandan keshari <chandankeshari93@gmail.com>
2022-01-28 02:31:55 +0530

    uploading files

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+         primary: '#f63',
+         secondary:{
+             100:'#E2E2D5',
+             200:'888883',
+         }
+      },
+      fontFamily:{
+        body:['Nunito','Permanent Marker'],
+      }
+    },
+  },
+  plugins: [],
+}
